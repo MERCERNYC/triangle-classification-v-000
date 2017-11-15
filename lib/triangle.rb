970d61446cb7671ed7941ae1654d2ac75e746f68
Juliana Fogg <julianafogg@gmail.com>
2017-11-15 20:34:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,26 @@
 class Triangle
-  
-  attr_accessor :side_one, :side_two, :side_three
 
-  def initialize (side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
-  
-end
+  attr_accessor :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+
+  def kind
+    if a == b && b==c #An equilateral triangle is a triangle that has three equal sides.
+    equilateral 
+    elsif a == b || a == c || b == c #An isosceles triangle is a triangle that has two equal sides. 
+    isosceles
+  else scalene #A scalene triangle is a triangle that has no equal sides
+      
+    
+  end
 
 class TriangleError < StandardError
 
   def message
      "Triangle is invalid"
-  end 
+  end
 end
